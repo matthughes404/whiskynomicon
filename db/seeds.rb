dc3dedddff84a3cb5ef7c508156cc8788f2747fb
Matt Hughes <matthughes.tech@gmail.com>
2015-07-15 15:10:29 -0400

    Add a bunch of Scotch to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 
 
 # brands and variants
+# bourbon
 Brand.create({ name: "Basil Hayden's",
   variants: [
     Variant.create({ style: 'Bourbon', proof: 80 })
@@ -81,7 +82,86 @@ Brand.create({ name: "Woodford Reserve",
     Variant.create({ style: 'Bourbon', proof: 90 })
   ]})
 
+# scotch
+Brand.create({ name: "Ardmore",
+  variants: [
+    Variant.create({ name: "Legacy", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Traditional Cask", style: 'Single malt Scotch', proof: 92 })
+  ]})
+Brand.create({ name: "Balvenie",
+  variants: [
+    Variant.create({ name: "Double Wood 12yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Single Barrel 12yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Caribbean Cask 14yr", style: 'Single malt Scotch', proof: 80 }),
+  ]})
+Brand.create({ name: "Bowmore",
+  variants: [
+    Variant.create({ style: '12yr', proof: 80 })
+  ]})
+Brand.create({ name: "Bunnahabhain",
+  variants: [
+    Variant.create({ style: '12yr', proof: 92 }),
+    Variant.create({ style: '18yr', proof: 92 })
+  ]})
+Brand.create({ name: "Cragganmore",
+  variants: [
+    Variant.create({ style: '12yr', proof: 80 })
+  ]})
+Brand.create({ name: "Glenfiddich",
+  variants: [
+    Variant.create({ name: "12yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "15yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "18yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "21yr", style: 'Single malt Scotch', proof: 80 })
+  ]})
+Brand.create({ name: "Glenlivet",
+  variants: [
+    Variant.create({ name: "12yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Nadurra 16yr", style: 'Single malt Scotch', proof: 96 }),
+    Variant.create({ name: "18yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "21yr", style: 'Single malt Scotch', proof: 80 })
+  ]})
+Brand.create({ name: "Glenmorrangie",
+  variants: [
+    Variant.create({ name: "12yr", style: 'Single malt Scotch', proof: 80 })
+  ]})
+Brand.create({ name: "Highland Park",
+  variants: [
+    Variant.create({ name: "12yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "18yr", style: 'Single malt Scotch', proof: 86 }),
+    Variant.create({ name: "21yr", style: 'Single malt Scotch', proof: 91 })
+  ]})
+Brand.create({ name: "Isle of Jura",
+  variants: [
+    Variant.create({ name: "Origin 10yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Elixir 12yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Diurach's Own 16yr", style: 'Single malt Scotch', proof: 80 })
+  ]})
+Brand.create({ name: "Lagavulin",
+  variants: [
+    Variant.create({ name: "16yr", style: 'Single malt Scotch', proof: 80 })
+  ]})
+Brand.create({ name: "Laphroaig",
+  variants: [
+    Variant.create({ name: "10yr", style: 'Single malt Scotch', proof: 80 }),
+    Variant.create({ name: "Triple Wood", style: 'Single malt Scotch', proof: 96 }),
+    Variant.create({ name: "18yr", style: 'Single malt Scotch', proof: 96 })
+  ]})
+Brand.create({ name: "Oban",
+  variants: [
+    Variant.create({ name: "14yr", style: 'Single malt Scotch', proof: 96 })
+  ]})
+Brand.create({ name: "Scapa",
+  variants: [
+    Variant.create({ name: "16yr", style: 'Single malt Scotch', proof: 96 })
+  ]})
+Brand.create({ name: "Speyburn",
+  variants: [
+    Variant.create({ name: "Bradan Orach", style: 'Single malt Scotch', proof: 80 })
+    Variant.create({ name: "10yr", style: 'Single malt Scotch', proof: 80 })
+  ]})
 
+# user, taste, and bottle
 User.create({ username: 'GenBurnside', name:'Matt Hughes', email: 'matthughes.tech@gmail.com' })
 Taste.create({ user_id: 1, brand: 'Buffalo Trace', variant: '', style: 'Bourbon', proof: 80,
   location: 'Home', rating: 4, review: 'Pretty tasty and affordable bourbon', date: DateTime.new(2015,7,13,18,0,0) })
