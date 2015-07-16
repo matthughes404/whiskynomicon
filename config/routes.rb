d7ca154d277bf1b335dd3ac45cb89e50d0bcfc4e
Matt Hughes <matthughes.tech@gmail.com>
2015-07-16 11:53:22 -0400

    Remove duplicate variant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'variants/index,'
-
-  get 'variants/create,'
-
-  get 'variants/show,'
-
-  get 'variants/update,'
-
-  get 'variants/destroy'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
