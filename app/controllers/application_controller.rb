f0c5e286b91cfd55ccc146a27f08b817ef9b1aa6
Matt Hughes <matthughes.tech@gmail.com>
2015-07-16 16:25:13 -0400

    Add error serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  # from http://jaketrent.com/post/jsonapi-errors-serializer-in-rails/
+  def error_output(errors)
+    return if errors.nil?
+
+    json = {}
+    new_hash = errors.to_hash(true).map do |k, v|
+      v.map do |msg|
+        { id: k, title: msg }
+      end
+    end.flatten
+    json[:errors] = new_hash
+    json
+  end
 end
