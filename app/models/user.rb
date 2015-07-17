5d40c54b347aa60a449a7fdda89822d15cdc4c55
Matt Hughes <matthughes.tech@gmail.com>
2015-07-17 17:21:18 -0400

    Disable e-mail confirm and OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :recoverable, :rememberable, :trackable, :validatable
+
   include DeviseTokenAuth::Concerns::User
-  
+
   has_many :tastes
   has_many :bottles
 end
