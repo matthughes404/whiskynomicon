fcf781ce9ee830db391378f2f7c48af2539fa407
Matt Hughes <matthughes.tech@gmail.com>
2015-07-17 14:44:59 -0400

    Add valiation to Taste model

--- app/models/taste.rb
+++ app/models/taste.rb
@@ -1,3 +1,9 @@
+require 'date_validator'
+
 class Taste < ActiveRecord::Base
   belongs_to :user
+  validates :brand, :presence => true
+  validates :style, :presence => true
+  validates :rating, :inclusion => { in: 0..5, message: 'must be between 0 and 5' }
+  validates :date, :date => { before: Proc.new { Date.tomorrow }, message: 'cannot be in the future' }
 end
