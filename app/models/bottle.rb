99aac35ca647b55c3a846fa4a7891f2510eebda2
matt <matthughes.tech@gmail.com>
2015-07-18 00:24:02 -0400

    Add validation to Bottle model

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -2,5 +2,6 @@ class Bottle < ActiveRecord::Base
   belongs_to :user
   validates :brand, :presence => true
   validates :style, :presence => true
-  validates :rating, :presence => true
+  validates :rating, :inclusion => { in: 0..5, message: 'must be between 0 and 5' }
+  validates :purchase_date, :date => { before: Proc.new { Date.tomorrow }, message: 'cannot be in the future' }
 end
