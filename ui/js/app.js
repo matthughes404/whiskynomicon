ea97c63d78f41efeff86e6f92a8667972e844aa0
matt <matthughes.tech@gmail.com>
2015-07-19 22:34:13 -0400

    Add structure for services

---
+++ ui/js/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('dramApp', ['dramServices']);
+app.constant('endpoint', 'http://localhost:3000/');
---
+++ ui/js/services.js
@@ -0,0 +1,89 @@
+var app = angular.module('dramServices', ['http']);
+
+app.service('brandService', function($http) {
+  this.getList = function() {
+
+  };
+
+  this.get = function(id) {
+
+  };
+
+  this.add = function(brand) {
+
+  };
+
+  this.update = function(id, brand) {
+
+  };
+
+  this.delete = function(id) {
+
+  };
+});
+
+app.service('variantService', function($http) {
+  this.getList = function() {
+
+  };
+
+  this.get = function(id) {
+
+  };
+
+  this.add = function(variant) {
+
+  };
+
+  this.update = function(id, variant) {
+
+  };
+
+  this.delete = function(id) {
+
+  };
+});
+
+app.service('tasteService', function($http) {
+  this.getList = function(user) {
+
+  };
+
+  this.get = function(user, id) {
+
+  };
+
+  this.add = function(user, taste) {
+
+  };
+
+  this.update = function(user, id, taste) {
+
+  };
+
+  this.delete = function(user, id) {
+
+  };
+});
+
+app.service('bottleService', function($http) {
+  this.getList = function(user) {
+
+  };
+
+  this.get = function(user, id) {
+
+  };
+
+  this.add = function(user, bottle) {
+
+  };
+
+  this.update = function(user, id, bottle) {
+
+  };
+
+  this.delete = function(user, id) {
+
+  };
+});
